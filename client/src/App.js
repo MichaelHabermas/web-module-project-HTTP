e1009513bc61994e2cf8108a8950c66b0c387b6f
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-19 16:09:02 -0400

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from 'react-router-dom';
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
 
-import EditMovieForm from './components/EditMovieForm';
+import EditMovieForm from './components/EditMovieForm.js';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
 import axios from 'axios';
 
-const App = (props) => {
-  const [movies, setMovies] = useState([]);
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
+const App = props => {
+	const [movies, setMovies] = useState([]);
+	const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/movies')
-      .then(res => {
-        setMovies(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get('http://localhost:5000/api/movies')
+			.then(res => {
+				setMovies(res.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, []);
 
-  const deleteMovie = (id)=> {
-  }
+	const deleteMovie = id => {
+		console.log(id);
+	};
 
-  const addToFavorites = (movie) => {
-    
-  }
+	const addToFavorites = movie => {
+		console.log(movie);
+	};
 
-  return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
-      </nav>
+	return (
+		<div>
+			<nav className="navbar navbar-dark bg-dark">
+				<span className="navbar-brand">
+					<img width="40px" alt="" src="./Lambda-Logo-Red.png" /> HTTP / CRUD Module Project
+				</span>
+			</nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
-          <Switch>
-            <Route path="/movies/edit/:id">
-            </Route>
+			<div className="container">
+				<MovieHeader />
+				<div className="row ">
+					<FavoriteMovieList favoriteMovies={favoriteMovies} />
 
-            <Route path="/movies/:id">
-              <Movie/>
-            </Route>
+					<Switch>
+						{/* <Route path="/movies/edit/:id" render= {props => {return <EditMovieForm>} /> */}
+						<Route path="/movies/edit/:id" render={props => <EditMovieForm setMovies={setMovies} />} />
 
-            <Route path="/movies">
-              <MovieList movies={movies}/>
-            </Route>
+						<Route path="/movies/:id">
+							<Movie />
+						</Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </div>
-  );
+						<Route path="/movies">
+							<MovieList movies={movies} />
+						</Route>
+
+						<Route path="/">
+							<Redirect to="/movies" />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-
 export default App;
-
